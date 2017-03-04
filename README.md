46054b6b1651d99665386224e2ba9fb8f83c2a82
Fangchang Ma <fcma@mit.edu>
2017-03-04 16:57:29 -0500

    save changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # docker_images
 docker images for TensorFlow and Torch
+
+## Usage
+Assume nvidia-docker has been installed.
+
+    $ cd ~/docker_images/<image-name>
+    $ nvidia-docker build -t <image-name> .
+
--- launchTF.sh
+++ launchTF.sh
@@ -1,1 +1,1 @@
-srun -c1 --pty --gres=gpu:1 nvidia-docker run --rm -v ~/work/:/work -ti tensorflow python /work/TensorFlow-Examples/examples/1_Introduction/helloworld.py
+srun -c1 --pty --gres=gpu:1 nvidia-docker run --rm -v ~/work/:/work -v ~/models/:/models -ti tensorflow python /work/TensorFlow-Examples/examples/3_NeuralNetworks/autoencoder.py
