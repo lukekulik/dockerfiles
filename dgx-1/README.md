b91b67599efaa2e3cec2949ffe0ee1f742ed84db
Fangchang Ma <fcma@mit.edu>
2017-08-23 17:05:45 -0400

    moved original torch dockerfile to under dgx-1 folder; created new general dockerfile for torch

---
+++ dgx-1/README.md
@@ -0,0 +1,1 @@
+Refer to http://docs.nvidia.com/deeplearning/dgx
--- launchTF.sh
+++
@@ -1,1 +0,0 @@
-srun -c1 --pty --gres=gpu:1 nvidia-docker run --rm -v ~/work/:/work -v ~/models/:/models -ti tensorflow python /work/TensorFlow-Examples/examples/3_NeuralNetworks/autoencoder.py
