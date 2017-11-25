a01f3469cf55a84970b25e360d21339fad8296ec
lukekulik <9;+f%9,vDx728vnF[Zr2/m^92)4?8&>
2017-11-25 15:48:39 -0500

    Update build file

--- deeplearning/build.sh
+++ deeplearning/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $0 = "cpu" ]; then
+if [ $1 = "cpu" ]; then
     docker build -t lkulik/deeplearning-cpu -f Dockerfile.cpu .
 fi
 
-if [ $0 = "gpu" ]; then
+if [ $1 = "gpu" ]; then
     nvidia-docker build -t lkulik/deeplearning-gpu -f Dockerfile.gpu .
 fi
