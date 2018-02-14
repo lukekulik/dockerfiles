c33b76df4f31d0bca09787780c81a1d0841648f4
Luke Kulik <luke.kulik@icloud.com>
2018-02-14 14:39:12 -0500

    Update build.sh

--- deeplearning/build.sh
+++ deeplearning/build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-if [ $1 = "cpu" ]; then
+choice="both"
+
+
+if [ ${1:-$choice} = "cpu" ]; then
     docker build -t lkulik/deeplearning-cpu -f Dockerfile .
 fi
 
-if [ $1 = "gpu" ]; then
+if [ ${1:-$choice} = "gpu" ]; then
     nvidia-docker build -t lkulik/deeplearning-gpu -f Dockerfile.gpu .
+fi
+if [ ${1:-$choice} = "both" ]; then
+    nvidia-docker build -t lkulik/deeplearning-gpu -f Dockerfile.gpu .
+    docker build -t lkulik/deeplearning-cpu -f Dockerfile .
 fi
