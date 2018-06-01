9f417c078affb30bda5185926d671c2b7014cef4
Luke Kulik <luke.kulik@icloud.com>
2018-06-01 13:04:45 -0400

    Update jupyter_notebook_config.py

--- deeplearning/jupyter_notebook_config.py
+++ deeplearning/jupyter_notebook_config.py
@@ -18,4 +18,4 @@ from IPython.lib import passwd
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
-c.MultiKernelManager.default_kernel_name = 'python2'
+c.MultiKernelManager.default_kernel_name = 'python3'
