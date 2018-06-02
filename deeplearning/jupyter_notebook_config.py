ac378c4bb1676e501bace66b67082e334c50c0cd
Luke Kulik <luke.kulik@icloud.com>
2018-06-02 00:17:19 -0400

    Update jupyter_notebook_config.py

--- deeplearning/jupyter_notebook_config.py
+++ deeplearning/jupyter_notebook_config.py
@@ -10,6 +10,6 @@ c.MultiKernelManager.default_kernel_name = 'python3'
 # Password to use for web authentication
 c.NotebookApp.password = u'sha1:95672cd645aa:73778711ea91d19230826efd2644f6e0f0cb466e'
 # The full path to an SSL/TLS certificate file.
-c.NotebookApp.certfile = u'/mycert.crt'
+c.NotebookApp.certfile = u'/etc/ssl/cert.pem'
 # The full path to a private key file for usage with SSL/TLS.
-c.NotebookApp.keyfile = u'/mycert.key'
+c.NotebookApp.keyfile = u'/etc/ssl/key.pem'
